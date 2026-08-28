3734de2d0aaad6473e679b5526f290883e7d570b
STiAT <ggrabler@gmail.com>
2026-08-28 20:13:38 +0200

    Initial bluebuild build

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,7 +13,12 @@ cp -avf "/ctx/system_files"/. /
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux
+dnf5 install -y \
+    @cosmic-desktop \
+    cosmic-monitor
+
+# add cosmic flatpak remote
+flatpak remote-add --if-not-exists --system https://apt.pop-os.org/cosmic/cosmic.flatpakrepo
 
 # Use a COPR Example:
 #
@@ -25,3 +30,4 @@ dnf5 install -y tmux
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl enable cosmic-greeter
